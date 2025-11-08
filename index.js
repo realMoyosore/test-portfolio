cf950a619b8278c46b5b0af97f41ffe0c388350e
Moyosore Sulola <moyixxoreoluwa@gmail.com>
2025-11-08 10:54:17 -1200

    updated

--- index.js
+++ index.js
@@ -38,10 +38,10 @@ window.addEventListener('scroll', () => {
     }
   });
 
-  // navLinks.querySelectorAll('a').forEach(link => {
+  navLinks.querySelectorAll('a').forEach(link => {
   //   link.classList.remove('active');
-  //   if (link.getAttribute('href') === `#${current}`) {
+    if (link.getAttribute('href') === `#${current}`) {
   //     link.classList.add('active');
-  //   }
+    }
   });
 });
