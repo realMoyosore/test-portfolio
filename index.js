76751ad6ed600287c0ff3a16a58ef52d8b1a723e
Moyosore Sulola <moyixxoreoluwa@gmail.com>
2025-11-10 00:07:55 +0100

    erase function

--- index.js
+++ index.js
@@ -65,3 +65,16 @@ function type() {
     setTimeout(erase, newTextDelay);
   }
 }
+function erase() {
+  if (charIndex > 0) {
+    if (!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex - 1);
+    charIndex--;
+    setTimeout(erase, erasingDelay);
+  } else {
+    cursorSpan.classList.remove("typing");
+    textArrayIndex++;
+    if (textArrayIndex >= textArray.length) textArrayIndex = 0;
+    setTimeout(type, typingDelay + 1100);
+  }
+}
