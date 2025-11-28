bf5f49bfb6f2a4664cdaf513ab5b1dab86ae4b19
Moyosore Sulola <moyixxoreoluwa@gmail.com>
2025-11-28 02:20:49 +0100

    image.foreach

--- index.js
+++ index.js
@@ -133,3 +133,12 @@ const images = document.querySelectorAll('.lightbox-image');
 const lightbox = document.createElement('div');
 lightbox.id = 'lightbox';
 document.body.appendChild(lightbox);
+images.forEach(image => {
+  image.addEventListener('click', e => {
+    lightbox.classList.add('active');
+    const img = document.createElement('img');
+    img.src = e.target.src;
+    lightbox.appendChild(img);
+  });
+});
+
