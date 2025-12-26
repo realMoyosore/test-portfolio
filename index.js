f127efd7b7a4df336e8cc9a8443f187002799307
Moyosore Sulola <moyixxoreoluwa@gmail.com>
2025-12-26 20:09:47 +0100

    break

--- index.js
+++ index.js
@@ -172,13 +172,7 @@ faders.forEach(fader => {
 // Back to top button functionality
 const backToTopButton = document.querySelector('.back-to-top');
 
-window.addEventListener('scroll', () => {
-  if (window.pageYOffset > 300) {
-    backToTopButton.classList.add('show');
-  } else {
-    backToTopButton.classList.remove('show');
-  }
-});
+
 
 backToTopButton.addEventListener('click', () => {
   window.scrollTo({
