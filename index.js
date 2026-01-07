dc22b93f152d79aa7f47b380a2025680464774ea
Moyosore Sulola <moyixxoreoluwa@gmail.com>
2026-01-07 13:15:19 +0100

    fix

--- index.js
+++ index.js
@@ -131,7 +131,13 @@ handleScrollAnimation();
 // Add a simple lightbox effect for images
 const images = document.querySelectorAll('.lightbox-image');
 const lightbox = document.createElement('div');
-
+lightbox.id = 'lightbox';
+document.body.appendChild(lightbox);
+images.forEach(image => {
+  image.addEventListener('click', e => {
+    lightbox.classList.add('active');
+    const img = document.createElement('img');
+    img.src = e.target.src;
     lightbox.appendChild(img);
   });
 });
